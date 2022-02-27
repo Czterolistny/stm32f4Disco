08bfdcf627edb94365db1677604eeb5875ec7339
Czterolistny <strzeciak3@gmail.com>
2022-02-27 18:53:24 +0100

    CO_Module clock init

--- proj/co/stm32f030_board_co/main.c
+++ proj/co/stm32f030_board_co/main.c
@@ -1,9 +1,15 @@
 #include "stm32f0xx.h"
+#include "stm32f0xx_gpio.h"
+#include "stm32f0xx_usart.h"
 
+void InitUsart1(void)
+{
+}
 
 int main()
 {
-	
+	SystemInit();
+    SysTick_Config(SystemCoreClock / 1000);
 	
 	for(;;);
 }
