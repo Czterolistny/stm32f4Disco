ecdbaaa9794debce608ad66e09ee7a09a203c834
Czterolistny <strzeciak3@gmail.com>
2022-03-20 19:38:26 +0100

    CO commands handling & processing implement
    AT-command to ESP import

--- proj/co/stm32f030_board_co/main.c
+++ proj/co/stm32f030_board_co/main.c
@@ -3,11 +3,66 @@
 #include "stm32f0xx_usart.h"
 #include "stm32f0xx_rcc.h"
 #include "stm32f0xx_misc.h"
+#include "stm32f0xx_tim.h"
 #include "sregs.h"
 #include "debugPins.h"
+#include "esp.h"
+
+#define FAN_PERC_ADDR 	((uint8_t) 0x3F)
+#define SET_TEMP_ADDR 	((uint8_t) 0x23)
+#define TEMP1_ADDR 		((uint8_t) 0x1E)
+#define TEMP2_ADDR 		((uint8_t) 0x26)
+#define EXH_TEMP_ADDR 	((uint8_t) 0x42)
+
+#define NULL ((void*)0) 
+
+const uint8_t param[] = {FAN_PERC_ADDR, SET_TEMP_ADDR, TEMP1_ADDR, TEMP2_ADDR, EXH_TEMP_ADDR};
+
+const uint8_t uart_respond[] = {0x02, 0x26, 0xff, 0xf4, 0x16, 0xf9, 0x00, 0x01, 0x16, 0xc2, 0x00, 0x00, 0x16, 0xf9, 0x00, 0x00,
+								0x16, 0xf9, 0x00, 0x02, 0x16, 0xc2, 0x00, 0x00, 0x16, 0xf9, 0x00, 0x00, 0x02, 0x18, 0x2c, 0x11};
+uint8_t *pTX_BUF = NULL;
+uint8_t rx_buf[128];
+
+volatile int8_t delay_5ms_ticks = -1;
+volatile uint8_t frame_byte_rec;
+volatile uint8_t tx_cnt;
+
+volatile uint32_t msTicks;
 
 void delay_ms(uint32_t ms);
 static void uartSend(uint8_t *tx_buf, uint8_t len);
+
+void USART1_IRQHandler(void)
+{
+    if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
+    {
+		USART_ClearFlag(USART1, USART_IT_RXNE);
+		rx_buf[frame_byte_rec++] = (uint8_t) USART_ReceiveData(USART1);
+		delay_5ms_ticks = 0;
+		
+    }else if(USART_GetITStatus(USART1, USART_IT_TXE) != RESET)
+    {
+		static uint8_t txed_cnt;
+		if(txed_cnt == tx_cnt){
+			USART_ITConfig(USART1, USART_IT_TXE, DISABLE);
+			txed_cnt = 0;
+			tx_cnt = 0;
+		}else {
+			USART_SendData(USART1, pTX_BUF[++txed_cnt]);
+		}
+    }
+}
+
+void TIM2_IRQHandler()
+{
+    if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
+    {
+		if( delay_5ms_ticks >= 0 ){
+			delay_5ms_ticks++;
+		}
+		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+    }
+}
 
 void testUart1(void)
 {
@@ -22,6 +77,14 @@ void testUart1(void)
 	}
 }
 
+static void uartSendToCO(uint8_t *tx_buf, uint8_t len)
+{
+	pTX_BUF = tx_buf;
+	USART_SendData(USART1, tx_buf[0]);
+	tx_cnt = len - 1;
+	USART_ITConfig(USART1, USART_IT_TXE, ENABLE);
+}
+
 static void uartSend(uint8_t *tx_buf, uint8_t len)
 {
 	for(uint8_t i = 0; i < len; ++i)
@@ -29,6 +92,38 @@ static void uartSend(uint8_t *tx_buf, uint8_t len)
 		while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
 		USART_SendData(USART1, tx_buf[i]);
 	}
+}
+
+void processData(uint8_t *recv_buf, uint8_t recv_len)
+{
+	if( recv_len > 0x20 )
+	{
+		sendToESP(recv_buf, recv_len);
+	}	
+	uartSendToCO((uint8_t *) &uart_respond[0], sizeof(uart_respond));
+}
+
+void TIM2_Init()
+{
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+	//10ms
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
+    TIM_TimeBaseInitStruct.TIM_Prescaler = 999;
+    TIM_TimeBaseInitStruct.TIM_Period = 419;
+    TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
+
+    TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStruct);
+
+    NVIC_InitTypeDef NVIC_InitStruct;
+    NVIC_InitStruct.NVIC_IRQChannel = TIM2_IRQn;
+    NVIC_InitStruct.NVIC_IRQChannelPriority = 0;
+    NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStruct);
+	
+	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+    TIM_Cmd(TIM2, ENABLE);
 }
 
 void InitUsart1(void)
@@ -72,8 +167,6 @@ void InitUsart1(void)
 	USART_ITConfig(USART1, USART_IT_TXE, DISABLE);
 }
 
-volatile uint32_t msTicks;
-
 void delay_ms(uint32_t ms)
 {
 	uint32_t ticks = msTicks + ms;
@@ -94,8 +187,13 @@ int main()
 	initTestPin();
 	
 	InitUsart1();
+	TIM2_Init();
 	//sregsInit();
 	
-	testUart1();
-	for(;;);
+	for (;;){
+		while( delay_5ms_ticks < 10 );
+		delay_5ms_ticks= -1;
+		processData(&rx_buf[0], frame_byte_rec);
+		frame_byte_rec = 0;
+	};
 }
