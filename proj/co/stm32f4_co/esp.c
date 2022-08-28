837b36953cbb8bdb8ec02a8e60974fb4633c9184
Czterolistny <strzeciak3@gmail.com>
2022-08-28 17:17:10 +0200

    Fix ESP CIPSEND
    esp.c refactoring

--- proj/co/stm32f4_co/esp.c
+++ proj/co/stm32f4_co/esp.c
@@ -5,19 +5,19 @@
 	#include "stm32f0xx_usart.h"
 #endif
 #include <stdint.h>
-#include "main.h"
 #include "esp.h"
 #include "../../common/common.h"
 
-#define CRC_INIT 0xB169
+#define ESP_CRC_INIT 	(0xB169)
+#define ESP_CRC_POLY 	(0x2f5c)
+const char startSign = ' ';
 
 extern const uint8_t param[];
 const uint8_t estabUDPInitcmd[] = {'A', 'T', '+', 'C', 'I', 'P', 'S', 'T', 'A', 'R', 'T', '=', '"', 'U', 'D', 'P', \
-                                    '"', ',', '"', '1', '9', '2', '.', '1', '6', '8', '.', '1', '.', '1', '0', '5', \
+                                    '"', ',', '"', '1', '9', '2', '.', '1', '6', '8', '.', '0', '.', '1', '0', '5', \
                                     '"', ',', '8', '9', '0', '1', ',', '8', '0', '0', '1', '\r', '\n'};
 
-static uint16_t crc16calc(char *buf, uint16_t crc, uint16_t len);
-inline static void sendATcmd(char *cmd, uint8_t len);
+inline static void espSendATcmd(char *cmd, uint8_t len);
 
 const espConfig *config;
 
@@ -34,60 +34,44 @@ void espInit(espConfig *conf)
 	delay_ms(1);
 }
 
-static void sendATcmd(char *cmd, uint8_t len)
+static void espSendATcmd(char *cmd, uint8_t len)
 {
 	(*config->espWriteATCommand)((uint8_t*) cmd, len);
 }
 
-void sendToESP(uint8_t *recv_buf, uint8_t recv_len)
+static uint16_t espGetCRC(char *buf, uint8_t len)
+{
+	uint16_t crc16 = crc16calc(&startSign, ESP_CRC_INIT, 1, ESP_CRC_POLY);
+	return crc16calc(&buf[0], crc16, len, ESP_CRC_POLY);
+}
+
+void espWrite(CoParamType *p)
 {
 	char udp_buf[32];
-	
-	uint8_t buf_len = uint_to_string(&udp_buf[0], recv_buf[param[SET_TEMP]]);
-	buf_len += uint_to_string(&udp_buf[buf_len], ((recv_buf[param[TEMP1]] << 8) | recv_buf[param[TEMP1] +1]) / 10);
-	buf_len += uint_to_string(&udp_buf[buf_len], ((recv_buf[param[TEMP2]] << 8) | recv_buf[param[TEMP2] +1]) / 10);
-	buf_len += uint_to_string(&udp_buf[buf_len], ((recv_buf[param[EXH_TEMP]] << 8) | recv_buf[param[EXH_TEMP] +1]) / 10);
-	buf_len += uint_to_string(&udp_buf[buf_len], recv_buf[param[FAN_PERC]]);
-	udp_buf[buf_len++] = '\n';
-	
-	char space = ' ';
-	uint16_t crc16 = crc16calc(&space, CRC_INIT, 1);
-	crc16 = crc16calc(&udp_buf[0], crc16, buf_len);
-	char crc16_str[5] = "0";
-	uint8_t crc16_len = uint_to_string(&crc16_str[0], crc16);
-	
+	char crc16_str[5] = {0};
 	char at_cmd[18] = "AT+CIPSEND=";
+
+	uint8_t udp_buf_len = 0u;
+
+	for(uint8_t paramCnt = 0; paramCnt < p->paramNmb; ++paramCnt)
+	{
+		udp_buf_len += uint_to_string(&udp_buf[udp_buf_len], p->param[paramCnt]);
+	}
+	udp_buf[udp_buf_len++] = '\n';
+	
+	uint16_t crc16 = espGetCRC(&udp_buf[0], udp_buf_len);
+	uint8_t crc16_len = uint_to_string(&crc16_str[0], crc16);
 	uint8_t at_len = _strlen(at_cmd);
-	at_len += uint_to_string(&at_cmd[at_len], buf_len + crc16_len);
+
+	at_len += uint_to_string(&at_cmd[at_len], udp_buf_len + crc16_len);
+
 	at_cmd[at_len -1] = '\r';
 	at_cmd[at_len++] = '\n';
 	
-	sendATcmd(&at_cmd[0], at_len);
-	/* TODO - Check for response */
-	sendATcmd(&crc16_str[0], crc16_len);
-	sendATcmd(&udp_buf[0], buf_len);
+	espSendATcmd(&at_cmd[0], at_len);
+	delay_ms(5);
+	/* TODO - Check for response - wait for comamnd prompt */
+	espSendATcmd(&crc16_str[0], crc16_len);
+	espSendATcmd(&udp_buf[0], udp_buf_len);
 	
-}
-
-static uint16_t crc16calc(char *buf, uint16_t crc, uint16_t len)
-{
-	uint16_t i, tmp_crc = crc, poly=0x2f5c;
-
-	while (len--)
-    {
-		tmp_crc = *buf++ << 8;
-	    crc = tmp_crc ^ crc;
-
-	    for(i = 0; i < 8; ++i)
-        {
-		    if(crc > 32767)
-			 	crc = poly ^ (crc << 1);
-            else
-			    crc = crc << 1;
-			
-			crc = crc & 0xFFFF;
-        }
-    }
-
-    return crc;
 }
--- proj/common/common.h
+++ proj/common/common.h
@@ -5,5 +5,6 @@
 
 uint8_t uint_to_string(char *s, uint16_t x);
 uint8_t _strlen(char *s);
+uint16_t crc16calc(const char *buf, uint16_t crc, uint16_t len, uint16_t poly);
 
 #endif
