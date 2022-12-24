811050eb794263094f7bed017d5c71f9e793278a
Czterolistny <strzeciak3@gmail.com>
2022-12-24 09:56:15 +0100

    ESP flash path fix

--- 3rdParty/ESP12F_BIN/flash_all.sh
+++ 3rdParty/ESP12F_BIN/flash_all.sh
@@ -2,6 +2,6 @@
 
 DEV=$1
 esptool.py --baud 115200 --port $DEV write_flash 0x00000 boot_v1.1.bin
-esptool.py --baud 115200 --port $DEV write_flash 0x01000 v0.20/user1.bin
+esptool.py --baud 115200 --port $DEV write_flash 0x01000 user1.bin
 esptool.py --baud 115200 --port $DEV write_flash 0xfc000 esp_init_data_default.bin
 esptool.py --baud 115200 --port $DEV write_flash 0xfe000 blank.bin
